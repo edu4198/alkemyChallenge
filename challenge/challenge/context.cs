ad85ee7d05f1af38be47755548a915409d804c7f
Eduardo Discioscia <71606119+edu4198@users.noreply.github.com>
2021-11-26 21:11:55 -0300

    Add files via upload

---
+++ challenge/challenge/context.cs
@@ -0,0 +1,21 @@
+﻿using challenge;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Context : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("");
+    }
+    public DbSet<Personaje> Personajes { get; set; }
+    public DbSet<PeliculaSerie> PeliculaSeries { get; set; }
+    public DbSet<Genero> Generos { get; set; }
+
+
+
+}
